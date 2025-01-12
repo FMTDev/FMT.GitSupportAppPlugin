442dcb3685d4cd4e7e2397baf9472094e27ab70b
Paulov-t <fmt-dev@outlook.com>
2025-01-12 08:57:37 +0000

    Add Notification interfaces and add Workflow to publish Nuget

---
+++ ConsumeNotificationOfAssetUpdated.cs
@@ -0,0 +1,13 @@
+﻿using FMT.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSupportAppPlugin
+{
+    //public class ConsumeNotificationOfAssetUpdated : FMT.PluginInterfaces.INotificationAssetUpdated
+    //{
+    //}
+}
